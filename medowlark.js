4ac4f061e524413a8817c8fce423f68db3023a07
rockn1989 <gold_100@bk.ru>
2019-02-11 22:41:17 +0300

    first project tests

--- medowlark.js
+++ medowlark.js
@@ -12,12 +12,20 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/publick'));
 
+app.use(function (req, res, next) {
+    res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+    next();
+});
+
 app.get('/', function (req, res) {
     res.render('home')
 });
 
 app.get('/about', function (req, res) {
-    res.render('about', { fortune: fortune.getFortune() });
+    res.render('about', {
+        fortune: fortune.getFortune(),
+        pageTestScript: 'qa/tests-about.js'
+    });
 });
 
 /*  404 Page */
