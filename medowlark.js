45e746da7de9ac81d630b239ded105b16c6e2f48
rockn1989 <gold_100@bk.ru>
2019-03-18 23:29:54 +0300

    add grunt files & QA

--- medowlark.js
+++ medowlark.js
@@ -1,7 +1,16 @@
 var express = require('express');
 
 var app = express();
-var handlebars = require('express-handlebars').create({ defaultLayout: 'main' });
+var handlebars = require('express-handlebars').create({
+    defaultLayout: 'main',
+    helpers: {
+        section: function (name, options) {
+            if (!this._sections) this._sections = {};
+            this._sections[name] = options.fn(this);
+            return null;
+        }
+    }
+});
 var fortune = require('./libs/fortune.js');
 
 app.engine('handlebars', handlebars.engine);
@@ -18,14 +27,42 @@ app.use(function (req, res, next) {
 });
 
 app.get('/', function (req, res) {
-    res.render('home')
+    res.render('home');
+});
+
+app.get('/tours/hood-river', function (req, res) {
+    res.render('tours/hood-river');
+});
+
+app.get('/tours/request-group-rate', function (req, res) {
+    res.render('tours/request-group-rate');
+});
+
+app.get('/tours/oregon-coast', function (req, res) {
+    res.render('tours/oregon-coast');
 });
 
 app.get('/about', function (req, res) {
     res.render('about', {
         fortune: fortune.getFortune(),
-        pageTestScript: 'qa/tests-about.js'
+        pageTestScript: 'qa/tests-about.js',
+        style: req.query.style,
+        userid: req.cookies,
+        username: req.statusMessage,
     });
+});
+
+app.get('/test', function (req, res) {
+    res.type('text/plain');
+    res.send('Это текст');
+});
+
+app.get('/headers', function (req, res) {
+    res.set('Content-type', 'text/plain');
+    var s = '';
+    for (var name in req.headers)
+        s += name + ': ' + req.headers[name] + '\n';
+    res.send(s);
 });
 
 /*  404 Page */
@@ -44,7 +81,7 @@ app.use(function (err, req, res, next) {
 });
 
 app.listen(app.get('port'), function () {
-    console.log('Express start')
+    console.log('Express start');
 });
 
-/*  */
+if (app.thing == null) console.log('Бе-е!');
