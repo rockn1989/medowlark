acb967516fc6887b08204851d0a0e41252ec7158
rockn1989 <gold_100@bk.ru>
2022-08-23 22:50:53 +0300

    обработка форм, подключение ПО multioarty

--- meadowlark.js
+++ meadowlark.js
@@ -1,24 +1,52 @@
 const express = require("express");
 const { engine } = require("express-handlebars");
+const multiparty = require('multiparty');
 const handlers = require("./lib/handlers");
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.disable("x-powered-by");
+
 const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + "/public"));
 
-app.engine(".hbs", engine({ extname: ".hbs" }));
-app.set("view engine", ".hbs");
-
-app.disable("x-powered-by");
 
 app.get("/", handlers.home);
 
 app.get("/about", handlers.about);
 
+app.get("/newsletter-signup", handlers.newsletterSignup);
+app.get("/newsletter-signup/thank-you", handlers.newsletterSignupThankYou);
+app.post("/newsletter-signup/process", handlers.newsletterSignupProcess);
+app.post('/api/newsletter-signup', handlers.api.newsletterSignup);
+
+app.get('/contest/vacation-photo', handlers.vacationPhotoContest)
+app.post('/api/vacation-photo-contest/:year/:month', (req, res) => {
+  const form = new multiparty.Form();
+  form.parse(req, (err, fields, files) => {
+    handlers.api.vacationPhotoContest(req, res, fields, files);
+  });
+});
+
 app.use(handlers.notFound);
 
 app.use(handlers.serverError);
+
+app.engine(
+  ".hbs",
+  engine({
+    extname: ".hbs",
+    helpers: {
+      section: function (name, options) {
+        if (!this._sections) this._sections = {};
+        this._sections[name] = options.fn(this);
+        return null;
+      },
+    },
+  })
+);
+app.set("view engine", ".hbs");
 
 if (require.main === module) {
   app.listen(port, () => {
